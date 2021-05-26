161a68816f672a335005e0cfca39fffb4562df32
Ajdin <ajdintpc@gmail.com>
2021-05-26 23:16:57 +0200

    profile almost done

---
+++ app/src/main/java/ProfileController/MainActivity.java
@@ -0,0 +1,24 @@
+package ProfileController;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.sprm_project.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+    }
+
+    public void profile(View v){
+        startActivity(new Intent(getApplicationContext(), Profile.class));
+    }
+}
