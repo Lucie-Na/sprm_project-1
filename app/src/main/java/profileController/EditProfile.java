2e76ee65243e82c274fe48bf9a6f70b6e51e352f
Ajdin <ajdintpc@gmail.com>
2021-06-03 15:19:54 +0200

    new content, not finish

--- app/src/main/java/profileController/EditProfile.java
+++ app/src/main/java/profileController/EditProfile.java
@@ -2,7 +2,9 @@ package profileController;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.sprm_project.R;
 
@@ -12,5 +14,25 @@ public class EditProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+    }
+
+    public void editInformation(View v){
+        startActivity(new Intent(getApplicationContext(), EditProfile.class));
+    }
+
+    public void editEmail(View v){
+        startActivity(new Intent(getApplicationContext(), EditProfile.class));
+    }
+
+    public void resetPassword(View v){
+        startActivity(new Intent(getApplicationContext(), EditProfile.class));
+    }
+
+    public void logoutFromProfile(View v){
+        startActivity(new Intent(getApplicationContext(), EditProfile.class));
+    }
+
+    public void closeActivity(View v){
+        finish();
     }
 }
