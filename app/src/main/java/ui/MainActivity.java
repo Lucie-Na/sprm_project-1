550f0604d97d66c18400fdc52cf341ef429e7c6c
Vinc Mze <morizevincent@gmail.com>
2021-06-04 01:59:44 +0200

    Implemented topbar, bottombar and navigation drawer from external separate files

--- app/src/main/java/ui/MainActivity.java
+++ app/src/main/java/ui/MainActivity.java
@@ -1,38 +1,137 @@
 package ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
 import com.example.sprm_project.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.Objects;
 
 import profileController.Profile;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView welcomeMessage;
-    private MainActivityViewModel viewModel;
+    MainActivityViewModel viewModel;
+
+    NavController navController;
+    AppBarConfiguration mAppBarConfiguration;
+    DrawerLayout drawerLayout;
+    NavigationView navigationDrawer;
+    BottomNavigationView bottomNavigationView;
+    Toolbar toolbar;
+
+    TextView drawername, drawerfollows, drawersubs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.navigation_layout);
         viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
         viewModel.init();
-        checkIfSignedIn();
-        setContentView(R.layout.activity_main);
-        welcomeMessage = findViewById(R.id.welcome_message);
 
+        checkIfSignedIn();
+
+        initViews();
+        setupNavigation();
+
+    }
+
+    private void initViews() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationDrawer = findViewById(R.id.nav_view);
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        toolbar = findViewById(R.id.toolbar);
+        View headerView = navigationDrawer.getHeaderView(0);
+        drawername = findViewById(R.id.drawername);
+        drawerfollows = findViewById(R.id.drawerfollows);
+        drawersubs = findViewById(R.id.drawersubs);
+    }
+
+
+    private void setupNavigation() {
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.bottomnav_notifications, R.id.bottomnav_mainpage, R.id.bottomnav_notifications,
+                R.id.nav_profile, R.id.nav_travels, R.id.nav_documentation,
+                R.id.nav_tips, R.id.nav_jobs, R.id.nav_settings)
+                .setOpenableLayout(drawerLayout)
+                .build();
+        setBottomNavigationVisibility();
+
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        NavigationUI.setupWithNavController(navigationDrawer, navController);
+        setBottomNavigationVisibility();
+    }
+
+    private void setBottomNavigationVisibility() {
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+                switch (destination.getId()){
+                    default:
+                        bottomNavigationView.setVisibility(View.GONE);
+                        break;
+                    case R.id.bottomnav_mainpage:
+                    case R.id.bottomnav_messages:
+                        bottomNavigationView.setVisibility(View.VISIBLE);
+                    case R.id.bottomnav_notifications:
+                        bottomNavigationView.setVisibility(View.VISIBLE);
+                }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item);
     }
 
     private void checkIfSignedIn() {
         viewModel.getCurrentUser().observe(this, user -> {
             if (user != null) {
-                String message = "Welcome " + user.getDisplayName();
-                welcomeMessage.setText(message);
+                //drawername.setText(user.getDisplayName());
             } else
                 startLoginActivity();
         });
