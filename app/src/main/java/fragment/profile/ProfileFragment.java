7764d77fbdced34a29f5ef54965b9b56cec8f586
Lucie Naffien <lucie.naffien@etu.unistra.fr>
2021-06-22 22:18:33 +0200

    Add travel implementation

--- app/src/main/java/fragment/profile/ProfileFragment.java
+++ app/src/main/java/fragment/profile/ProfileFragment.java
@@ -1,5 +1,6 @@
 package fragment.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,12 +11,27 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.sprm_project.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import ui.AddTravelActivity;
 
 
 public class ProfileFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View currentView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        // initialize floating action button
+        FloatingActionButton fabAddTravel = currentView.findViewById(R.id.fab_add_travel);
+        fabAddTravel.setOnClickListener(view ->
+        {
+            Intent intent = new Intent(currentView.getContext(), AddTravelActivity.class);
+            // wait a result from the new activity
+            startActivity(intent);
+            //startActivityForResult(intent, NEW_BOOK_ACTIVITY_REQUEST_CODE);
+        });
+
+        return currentView;
     }
 }
--- app/src/main/java/ui/AddTravelActivity.java
+++ app/src/main/java/ui/AddTravelActivity.java
@@ -7,9 +7,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,8 +32,7 @@ import java.util.List;
 import data.api.LocationViewModel;
 import models.Institution;
 
-public class AddTravelActivity extends AppCompatActivity
-{
+public class AddTravelActivity extends AppCompatActivity {
     TextView textStartDate;
     TextView textEndDate;
 
@@ -44,10 +46,16 @@ public class AddTravelActivity extends AppCompatActivity
     SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_travel);
+
+        // initialize the back button
+        ImageButton buttonBack = findViewById(R.id.button_add_travel_back);
+        buttonBack.setOnClickListener(v ->
+        {
+            finish();
+        });
 
         // initialize the country spinner
         initializeCountry();
@@ -62,31 +70,39 @@ public class AddTravelActivity extends AppCompatActivity
         initializeDatesInput(textEndDate);
 
         // initialize the spinner with the duration type
-        Spinner spinnerDurationType = findViewById(R.id.spinner_add_travel_duration_type);
+        /*Spinner spinnerDurationType = findViewById(R.id.spinner_add_travel_duration_type);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.travel_duration_type_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerDurationType.setAdapter(adapter);
+        spinnerDurationType.setAdapter(adapter);*/
 
-        // initialize the next page button
-        findViewById(R.id.button_add_travel_next).setOnClickListener(v ->
+        // initialize the validation button
+        findViewById(R.id.button_add_travel_validate).setOnClickListener(v ->
         {
-            nextPage();
+            Toast.makeText(this, "Did you just filled the whole form for nothing ? Well... Yes. You should blame the developers for that !", Toast.LENGTH_LONG).show();
+            finish();
         });
 
     }
 
+    /**
+     * Initializes the country Spinner with an API
+     */
     private void initializeCountry()
     {
-        // country
         ArrayList<String> listCountry = new ArrayList<String>();
         Spinner spinnerCountry = findViewById(R.id.spinner_add_travel_country);
+
+        // set an adapter to set the api's data to the spinner
         ArrayAdapter<String> adapterCountry = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, listCountry);
         adapterCountry.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerCountry.setAdapter(adapterCountry);
+
+        // change the selected item when something the user click on it
         spinnerCountry.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 selectedCountry = parent.getItemAtPosition(position).toString();
+                Log.e("Add travel", "New country selected");
             }
 
             @Override
@@ -95,13 +111,13 @@ public class AddTravelActivity extends AppCompatActivity
             }
         });
 
-        // view model
+        // set a view model to fill the spinner with the wanted data
         locationViewModel = ViewModelProviders.of(this).get(LocationViewModel.class);
-        locationViewModel.getCountry().observe(this, new Observer<List<Institution>>()
-        {
+        locationViewModel.getCountry().observe(this, new Observer<List<Institution>>() {
             @Override
             public void onChanged(@Nullable List<Institution> listCountryParam)
             {
+                Log.e("Add travel", "Api data for the country changed");
                 for (Institution countryAPI : listCountryParam)
                 {
                     alreadyInTheSpinner(listCountry, countryAPI.getCountry());
@@ -111,42 +127,49 @@ public class AddTravelActivity extends AppCompatActivity
         });
     }
 
-    private void alreadyInTheSpinner(ArrayList<String> listToUpdate, String apiData)
-    {
+    /**
+     * Add a new country to the list if it is a new one
+     *
+     * @param listToUpdate list to add the country
+     * @param apiData
+     */
+    private void alreadyInTheSpinner(ArrayList<String> listToUpdate, String apiData) {
         boolean alreadyInTheSpinner = false;
 
-        for(String country : listToUpdate)
-        {
-            if(apiData.compareTo(country) == 0)
-            {
+        // check if the country isn't already in the spinner
+        for (String country : listToUpdate) {
+            if (apiData.compareTo(country) == 0) {
                 alreadyInTheSpinner = true;
             }
         }
 
-        if(!alreadyInTheSpinner)
-        {
+        // add the country to the spinner if it is not already in
+        if (!alreadyInTheSpinner) {
             listToUpdate.add(apiData);
         }
 
+        // sort the countries in the spinner by their alphabetical name
         Collections.sort(listToUpdate);
     }
 
+    /**
+     * Initializes the institution Spinner with an API
+     */
     private void initializeInstitution()
     {
         ArrayList<String> listInstitution = new ArrayList<String>();
         spinnerInstitution = findViewById(R.id.spinner_add_travel_institution);
+
+        // set an adapter to fill the spinner
         ArrayAdapter<String> adapterInstitution = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, listInstitution);
         adapterInstitution.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerInstitution.setAdapter(adapterInstitution);
 
-        locationViewModel.getInstitutionsByCountry(selectedCountry).observe(this, new Observer<List<Institution>>()
-        {
+        locationViewModel.getInstitutionsByCountry(selectedCountry).observe(this, new Observer<List<Institution>>() {
             @Override
-            public void onChanged(List<Institution> institutions)
-            {
+            public void onChanged(List<Institution> institutions) {
                 listInstitution.clear();
-                for(Institution institutionAPI : institutions)
-                {
+                for (Institution institutionAPI : institutions) {
                     listInstitution.add(institutionAPI.getName());
                 }
                 Collections.sort(listInstitution);
@@ -155,12 +178,17 @@ public class AddTravelActivity extends AppCompatActivity
         });
     }
 
-    private void initializeDatesInput(TextView textDate)
-    {
+    /**
+     * Initializes a date input
+     * @param textDate
+     */
+    private void initializeDatesInput(TextView textDate) {
         textDate.setOnClickListener(v ->
         {
+            // open a date picker
             DatePickerDialog.OnDateSetListener setDate = new DatePickerDialog.OnDateSetListener()
             {
+                // change the date text field according to the selected date from the date picker
                 @Override
                 public void onDateSet(DatePicker view, int year, int month, int dayOfMonth)
                 {
@@ -170,9 +198,11 @@ public class AddTravelActivity extends AppCompatActivity
                     startDate = stringToDate(textStartDate);
                     endDate = stringToDate(textEndDate);
 
+                    Log.e("Add travel", "A new travel date has been picked !");
+
                     // update status and duration
                     initializeStatus();
-                    initializeDuration();
+                    //initializeDuration();
                 }
             };
 
@@ -183,80 +213,72 @@ public class AddTravelActivity extends AppCompatActivity
         });
     }
 
+    /**
+     * Automatically changes the travel status according to the start and end dates
+     */
     private void initializeStatus()
     {
         TextView textStatus = findViewById(R.id.text_input_status);
-        Log.i("Debug", "initialize status begin");
-        if(!textStartDate.getText().toString().isEmpty() && !textEndDate.getText().toString().isEmpty())
+        Log.e("Add travel", "Initialize status : begin");
+        // check if the dates have been given
+        if (!textStartDate.getText().toString().isEmpty() && !textEndDate.getText().toString().isEmpty())
         {
             Date currentDate = new Date();
+            // relates the given dates according to the current date
             int comparisonStart = currentDate.compareTo(startDate);
             int comparisonEnd = currentDate.compareTo(endDate);
 
-            if ( comparisonStart == 0)
-            {
+            // set the travel status according to the given travel dates
+            if (comparisonStart == 0) {
                 Log.e("Debug", "comparisonStart == 0");
                 textStatus.setText(getResources().getString(R.string.travel_status_in_progress));
-            }
-            else if(comparisonStart < 0)
-            {
+            } else if (comparisonStart < 0) {
                 Log.e("Debug", "comparisonStart < 0");
                 textStatus.setText(getResources().getString(R.string.travel_status_in_preparation));
-            }
-            else if(comparisonStart > 0 && comparisonEnd <= 0)
-            {
+            } else if (comparisonStart > 0 && comparisonEnd <= 0) {
                 Log.e("Debug", "comparisonStart > 0 && comparisonEnd <= 0");
                 textStatus.setText(getResources().getString(R.string.travel_status_in_progress));
-            }
-            else if (comparisonEnd > 0)
-            {
+            } else if (comparisonEnd > 0) {
                 Log.e("Debug", "comparisonEnd > 0");
                 textStatus.setText(getResources().getString(R.string.travel_status_over));
-            }
-            else
-            {
+            } else {
                 textStatus.setText("error");
             }
         }
         else
-        {
+            {
             textStatus.setText("");
         }
+        Log.e("Add travel", "Status has been changed");
     }
 
-    private void initializeDuration()
-    {
-        if(!textStartDate.getText().toString().isEmpty() && !textEndDate.getText().toString().isEmpty())
-        {
-            if(startDate != null && endDate !=null)
-            {
+    /*private void initializeDuration() {
+        if (!textStartDate.getText().toString().isEmpty() && !textEndDate.getText().toString().isEmpty()) {
+            if (startDate != null && endDate != null) {
                 int intStartDate = startDate.getYear() * 12 + startDate.getMonth();
                 int intEndDate = endDate.getYear() * 12 + endDate.getMonth();
 
-                switch (intEndDate - intStartDate + 1)
-                {
+                switch (intEndDate - intStartDate + 1) {
                     default:
                         break;
                 }
 
             }
         }
-    }
+    }*/
 
-    private Date stringToDate(TextView textDate)
-    {
-        try
-        {
+    /**
+     * Converts a TextView's content to a date type
+     * @param textDate the textView's content to convert
+     * @return
+     */
+    private Date stringToDate(TextView textDate) {
+        try {
             String stringDate = (textDate.getText().toString());
             return dateFormat.parse(stringDate);
-        }
-        catch (ParseException e)
-        {
+        } catch (ParseException e) {
             return null;
         }
 
     }
-
-    private void nextPage() {
-
-    }
+}
